970b1b8b23a7b3e8870c934033859d3001285243
JairGo21 <jairgomez.bogota@gmail.com>
2022-12-16 22:19:57 -0500

    Django-CRUD-End

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Project, Task
+from .forms import CreateNewTask, CreateNewProject
 
 # Create your views here.
 def index(request):
@@ -22,13 +23,40 @@ def hello(request, username):
 def project(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request,'project.html', {
+    return render(request,'projects/project.html', {
         'projects':projects
     })
 
 def tasks(request):
     # task = get_object_or_404(Task, title=title)
     tasks = Task.objects.all()
-    return render(request, 'tasks.html', {
+    return render(request, 'tasks/tasks.html', {
+        'tasks': tasks
+    })
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'tasks/create_task.html',{
+        'form': CreateNewTask()
+        })
+    else:
+        Task.objects.create(title=request.POST['title'],
+        description = request.POST['description'], project_id=1)
+        return redirect('tasks')
+
+def create_project(request):
+    if request.method == 'GET': 
+        return render(request,'projects/create_project.html', {
+        'form': CreateNewProject()
+        })
+    else:
+        project = Project.objects.create(name=request.POST["name"])
+        return redirect('projects')
+
+def project_detail(request,id):
+    project = get_object_or_404(Project,id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/detail.html', {
+        'project': project,
         'tasks': tasks
     })
